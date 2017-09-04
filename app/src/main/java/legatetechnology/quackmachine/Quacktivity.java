eda130c38c83cf87c35664945e82f7805fa076b7
Kenneth C. LaMarca <klamarca@chessmh.com>
2017-09-04 17:01:57 -0600

    Animation improvments. Got rid of toast message bug. Other improvments.

--- app/src/main/java/legatetechnology/quackmachine/Quacktivity.java
+++ app/src/main/java/legatetechnology/quackmachine/Quacktivity.java
@@ -3,9 +3,13 @@ package legatetechnology.quackmachine;
 import android.animation.Animator;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
+import android.graphics.Path;
 import android.graphics.drawable.Drawable;
 import android.media.AudioManager;
 import android.media.Image;
@@ -16,8 +20,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
@@ -35,25 +42,32 @@ import java.util.HashMap;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class Quacktivity extends AppCompatActivity implements CircleLayout.OnItemSelectedListener, CircleLayout.OnItemClickListener, Animator.AnimatorListener, View.OnLongClickListener, View.OnClickListener, SeekBar.OnSeekBarChangeListener {
+public class Quacktivity extends AppCompatActivity implements CircleLayout
+        .OnItemSelectedListener, CircleLayout.OnItemClickListener, Animator.AnimatorListener,
+        View.OnLongClickListener, View.OnClickListener, SeekBar.OnSeekBarChangeListener {
     private int quack_factor = 0;
     private SeekBar seek_quackFactor;
     private TextView tv_quackFactor;
     private TextView tv_qTitle;
     private ImageButton ib_quacker;
+    private ImageButton ib_birdSelect;
     private CircleLayout circle_selector;
     private View mainLayout;
     private boolean menuState = true;
     private static final int QUACK_CONSTANT = 5;
     private AnimatorSet animSetXYOpen;
     private AnimatorSet animSetXYClose;
+    private AnimatorSet animSetTrans;
+    private AnimatorSet animSetTrans_n;
     private ViewGroup.LayoutParams origParams;
     private ViewGroup.LayoutParams noParams;
     private String birdState = "duck";
 
+
     //Quack sources
     private int[] quackSource = {R.raw.quack_n_5, R.raw.quack_n_4, R.raw.quack_n_3, R.raw.quack_n_2
-            , R.raw.quack_n_1, R.raw.quack_0, R.raw.quack_1, R.raw.quack_2, R.raw.quack_3, R.raw.quack_4, R.raw.quack_5};
+            , R.raw.quack_n_1, R.raw.quack_0, R.raw.quack_1, R.raw.quack_2, R.raw.quack_3, R.raw
+            .quack_4, R.raw.quack_5};
 
     private int gooseSource = R.raw.goose;
     private int pigeonSource = R.raw.pigeon;
@@ -63,6 +77,8 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
     private HashMap<Integer, Integer> gooseCall;
     private HashMap<Integer, Integer> pigeonCoo;
     private SoundPool mySound;
+
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -82,6 +98,7 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
         seek_quackFactor = (SeekBar) findViewById(R.id.seekBar);
         tv_quackFactor = (TextView) findViewById(R.id.tv_quackFactor);
         ib_quacker = (ImageButton) findViewById(R.id.ib_quacker);
+        ib_birdSelect = (ImageButton) findViewById(R.id.ib_birdSelect);
         circle_selector = (CircleLayout) findViewById(R.id.rMenu_context);
         tv_qTitle = (TextView) findViewById(R.id.tv_title);
         mainLayout = findViewById(R.id.main);
@@ -89,10 +106,12 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
         //Listeners
         ib_quacker.setOnClickListener(this);
         ib_quacker.setOnLongClickListener(this);
+        ib_birdSelect.setOnClickListener(this);
         seek_quackFactor.setOnSeekBarChangeListener(this);
         mainLayout.setOnClickListener(this);
         circle_selector.setOnItemClickListener(this);
         circle_selector.setOnItemSelectedListener(this);
+
 
         //initialize HashMap
         quacks = new HashMap<Integer, Integer>();
@@ -117,7 +136,6 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
         origParams = circle_selector.getLayoutParams();
         noParams = new RelativeLayout.LayoutParams(0,0);
 
-        initAnimations();
         //Fill the hash map with the resource id as the key
         //and the loaded quack audio as the value
         for (int res : quackSource) {
@@ -125,6 +143,12 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
         }
         gooseCall.put(gooseSource,mySound.load(this,gooseSource,1));
         pigeonCoo.put(pigeonSource,mySound.load(this,pigeonSource,1));
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        initAnimations();
     }
 
     /**
@@ -138,23 +162,30 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
 
     @Override
     public void onClick(View v) {
-        //On button click play audio based on seekbar progress
-        switch (v.getId()) {
-            case R.id.ib_quacker: {
-                if(menuState)
-                    animateClose();
-                else
-                    play_Quack();
-                break;
-            }
-            case R.id.rMenu_context:{
-                if(!menuState)
-                    break;
-            }
-        }
 
         if(menuState)
             animateClose();
+        //On button click play audio based on seekbar progress
+        else {
+            switch (v.getId()) {
+                case R.id.ib_quacker: {
+                    if (menuState)
+                        animateClose();
+                    else
+                        play_Quack();
+                    break;
+                }
+                case R.id.rMenu_context: {
+                    if (!menuState)
+                        break;
+                }
+                case R.id.ib_birdSelect: {
+                    if (!menuState)
+                        animateOpen();
+                    break;
+                }
+            }
+        }
     }
 
     @Override
@@ -169,6 +200,7 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
      */
 
     private void initAnimations(){
+
         ObjectAnimator animX = ObjectAnimator.ofFloat(circle_selector, "scaleX", 0f,1f);
         ObjectAnimator animY = ObjectAnimator.ofFloat(circle_selector, "scaleY", 0f,1f);
         animSetXYOpen = new AnimatorSet();
@@ -181,6 +213,49 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
         animSetXYClose.playTogether(animX_n,animY_n);
         animSetXYClose.addListener(this);
 
+        int[] circle_Pos = new int[2];
+
+        //Top left location of circle_selector
+        circle_selector.getLocationOnScreen(circle_Pos);
+        circle_Pos[0] += (circle_selector.getWidth()/2); //get center X
+        circle_Pos[1] += (circle_selector.getHeight()/2); //get center Y
+        System.out.println("circle_Pos : " + circle_Pos[0] + " , " + circle_Pos[1]);
+
+        //Top left location of ib_birdSelect
+        int[] ib_Pos = new int[2];
+        ib_birdSelect.getLocationOnScreen(ib_Pos);
+        ib_Pos[0]+=(ib_birdSelect.getWidth()/2); //get center X
+        ib_Pos[1]+=(ib_birdSelect.getHeight()/2); //get center Y
+        System.out.println("ib_Pos : " + ib_Pos[0] + " , " + ib_Pos[1]);
+
+
+        Configuration thisConfig = getResources().getConfiguration();
+
+        float[] offset = new float[2];
+
+        offset[0] = ib_Pos[0] - circle_Pos[0];
+        offset[1] = ib_Pos[1] - circle_Pos[1];
+        System.out.println("Offset : " + offset[0] + " , " + offset[1]);
+
+        ObjectAnimator transX;
+        ObjectAnimator transY;
+        ObjectAnimator transX_n;
+        ObjectAnimator transY_n;
+
+        transX = ObjectAnimator.ofFloat(circle_selector,"translationX",0f,offset[0]);
+        transY = ObjectAnimator.ofFloat(circle_selector,"translationY",0f,offset[1]);
+        transX_n = ObjectAnimator.ofFloat(circle_selector,"translationX",offset[0],
+                0f);
+        transY_n = ObjectAnimator.ofFloat(circle_selector,"translationY",offset[1],
+                0f);
+
+        animSetTrans = new AnimatorSet();
+        animSetTrans.playTogether(transX,transY);
+        animSetTrans.addListener(this);
+
+        animSetTrans_n = new AnimatorSet();
+        animSetTrans_n.playTogether(transX_n,transY_n);
+        animSetTrans_n.addListener(this);
     }
 
 
@@ -190,11 +265,13 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
     private void animateOpen() {
         menuState = true;
         animSetXYOpen.start();
+        animSetTrans_n.start();
     }
 
     private void animateClose(){
         menuState = false;
         animSetXYClose.start();
+        animSetTrans.start();
     }
 
     /**
@@ -225,23 +302,28 @@ public class Quacktivity extends AppCompatActivity implements CircleLayout.OnIte
                 seek_quackFactor.setVisibility(View.VISIBLE);
                 tv_qTitle.setVisibility(View.VISIBLE);
                 tv_quackFactor.setVisibility(View.VISIBLE);
+                ib_birdSelect.setImageResource(R.drawable.style_birdselect_duck);
                 break;
             case "goose":
                 ib_quacker.setImageResource(R.drawable.feature_goose);
                 seek_quackFactor.setVisibility(View.INVISIBLE);
                 tv_qTitle.setVisibility(View.INVISIBLE);
                 tv_quackFactor.setVisibility(View.INVISIBLE);
+                ib_birdSelect.setImageResource(R.drawable.style_birdselect_goose);
                 break;
             case "pigeon":
                 ib_quacker.setImageResource(R.drawable.feature_pigeon);
                 seek_quackFactor.setVisibility(View.INVISIBLE);
                 tv_qTitle.setVisibility(View.INVISIBLE);
                 tv_quackFactor.setVisibility(View.INVISIBLE);
+                ib_birdSelect.setImageResource(R.drawable.style_birdselect_pigeon);
                 break;
         }
         birdState = bird;
-        Toast toast = Toast.makeText(this, birdState, Toast.LENGTH_SHORT);
-        toast.show();
+        if(BuildConfig.DEBUG){
+            Toast toast = Toast.makeText(this, birdState, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     /**
